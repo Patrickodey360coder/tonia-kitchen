641c8c0daa1bebbcf4b5c58b19dbdce9e78813a7
patrickodey <patrickodey419@gmail.com>
2021-10-26 22:29:49 +0100

    fixed hero section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Nav from './components/Navbar'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Nav from './components/Navbar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Globalstyle from "./Globalstyle";
-import Hero from './components/Hero'
-import About from './pages/About'
-import Contact from './pages/Contact'
+import Hero from './components/Hero';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import NoMatch from "./components/NoMatch";
-import ViewDog from "./components/ViewDog";
 
 
 function App() {

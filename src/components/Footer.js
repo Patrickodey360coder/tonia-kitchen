09625091a42d8a4b761f9110f69c0a7aac4e34a8
patrickodey <patrickodey419@gmail.com>
2021-10-27 07:31:14 +0100

    EsLint

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer class="footer mt-auto py-3 bg-light">
-      <div class="container">
-        <span class="text-muted">Developed by Patrick Odey</span>
+    <footer className="footer mt-auto py-3 bg-light">
+      <div className="container">
+        <span className="text-muted text-center">Developed by Patrick Odey</span>
       </div>
   </footer>
   )

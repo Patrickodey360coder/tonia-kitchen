e0e908be1909a5cce47ab81372f56a93498c1834
patrickodey <patrickodey419@gmail.com>
2021-10-27 07:31:51 +0100

    EsLint

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer mt-auto py-3 bg-light">
       <div className="container">
-        <span className="text-muted text-center">Developed by Patrick Odey</span>
+        <div className="text-muted text-center">Developed by Patrick Odey</div>
       </div>
   </footer>
   )

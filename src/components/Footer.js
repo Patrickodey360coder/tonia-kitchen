25a64503f609134a49119a75a28959fb7d39b7ad
patrickodey <patrickodey419@gmail.com>
2021-10-27 07:59:49 +0100

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+// import { BrowserRouter as Router, a } from 'react-router-dom'
 
 function Footer() {
   return (
@@ -7,11 +7,9 @@ function Footer() {
       <div className="container">
         <div className="text-muted text-center my-1">Developed by Patrick Odey</div>
         <div className="text-center">
-          <Router>
-            <Link to="https://github.com/Patrickodey360coder" target="_blank"><i className="fab fa-github-square fa-2x mx-2"></i></Link>
-            <Link to="https://twitter.com/patrickodey_" target="_blank"><i class="fab fa-twitter-square fa-2x mx-2"></i></Link>
-            <Link to="https://www.linkedin.com/in/patrick-odey-a5289a184/" target="_blank"><i class="fab fa-linkedin fa-2x mx-2"></i></Link>
-          </Router>
+            <a href="https://github.com/Patrickodey360coder" target="_blank" rel="noreferrer"><i className="fab fa-github-square fa-2x mx-2"></i></a>
+            <a href="https://twitter.com/patrickodey_" target="_blank" rel="noreferrer" ><i class="fab fa-twitter-square fa-2x mx-2"></i></a>
+            <a href="https://www.linkedin.com/in/patrick-odey-a5289a184/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin fa-2x mx-2"></i></a>
         </div>
       </div>
   </footer>

c4375a2baed4ec2b8d462653b90f1fc70808cab1
patrickodey <patrickodey419@gmail.com>
2021-10-27 10:37:19 +0100

    About Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,11 +3,15 @@ import bg2 from '../images/bg2.jpg';
 
 function About() {
   return (
-    <div className="container my-5">
+    <div className="container my-5 contact">
       <div className="row my-5">
-        <div className="col-md-6">
-          <h1>About Us</h1>
-          <p>loremzz</p>
+        <div className="col-md-6 my-5">
+          <h1 className="p5">About Us</h1>
+          <p className="my-5 text-muted">At DogsHaven, we care for and admire dogs. On this platform, you can search for different breeds of dogs around the world. You can also find basic information about each breed of dog like, name, life span, purpose for breeding.</p>
+
+          <p className="text-muted">
+            This web app was built using React (react-hooks, react-router, styled-components). It is mobile responsive and fast.
+          </p>
         </div>
         <div className="col-md-6">
           <img class="custom-img-about" src={bg2} alt={bg2}/>

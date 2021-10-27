f6d0aac0be4da671ae8bbd083e30797e6902bb18
patrickodey <patrickodey419@gmail.com>
2021-10-27 07:39:49 +0100

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="footer mt-auto py-3 bg-dark">
+    <footer className="footer mt-auto py-3 bg-light">
       <div className="container">
         <div className="text-muted text-center">Developed by Patrick Odey</div>
-        <div></div>
+        <div className="text-center">
+          <i className="fab fa-github-square"></i>
+          <i class="fab fa-twitter-square"></i>
+          <i class="fab fa-linkedin"></i>
+        </div>
       </div>
   </footer>
   )

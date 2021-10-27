963f2e8e2f8aa333cccf74be4a5dcc9fdc5d1324
patrickodey <patrickodey419@gmail.com>
2021-10-27 07:50:11 +0100

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,9 @@ function Footer() {
         <div className="text-muted text-center my-1">Developed by Patrick Odey</div>
         <div className="text-center">
           <Router>
-            <Link><i className="fab fa-github-square fa-2x mx-2"></i></Link>
-            <Link><i class="fab fa-twitter-square fa-2x mx-2"></i></Link>
-            <Link><i class="fab fa-linkedin fa-2x mx-2"></i></Link>
+            <Link to="https://github.com/Patrickodey360coder" target="_blank"><i className="fab fa-github-square fa-2x mx-2"></i></Link>
+            <Link to="https://twitter.com/patrickodey_" target="_blank"><i class="fab fa-twitter-square fa-2x mx-2"></i></Link>
+            <Link to="https://www.linkedin.com/in/patrick-odey-a5289a184/" target="_blank"><i class="fab fa-linkedin fa-2x mx-2"></i></Link>
           </Router>
         </div>
       </div>

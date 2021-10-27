ed7dbbaed596dcdea839626a87a19e97f32cc6ee
patrickodey <patrickodey419@gmail.com>
2021-10-27 07:45:45 +0100

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 function Footer() {
   return (
     <footer className="footer mt-auto py-3 bg-light">
       <div className="container">
-        <div className="text-muted text-center">Developed by Patrick Odey</div>
+        <div className="text-muted text-center my-1">Developed by Patrick Odey</div>
         <div className="text-center">
-          <i className="fab fa-github-square"></i>
-          <i class="fab fa-twitter-square"></i>
-          <i class="fab fa-linkedin"></i>
+          <Router>
+            <Link><i className="fab fa-github-square fa-2x mx-2"></i></Link>
+            <Link><i class="fab fa-twitter-square fa-2x mx-2"></i></Link>
+            <Link><i class="fab fa-linkedin fa-2x mx-2"></i></Link>
+          </Router>
         </div>
       </div>
   </footer>

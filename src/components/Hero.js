8501985aca868e0c926b331cc6079adf24ac4554
patrickodey <patrickodey419@gmail.com>
2021-11-01 23:49:35 +0100

    banga

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,6 +10,7 @@ import egusi from '../images/egusi.jpg';
 import okro from '../images/egusi.jpg';
 import vegetable from '../images/vegetable.jpg';
 import whitesoup from '../images/whitesoup.jpg';
+import banga from '../images/banga.jpg';
 
 function Hero() {
   return (
@@ -73,7 +74,7 @@ function Hero() {
                 <Food name="Okra (Beef or Goat Meat)" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N15,000" price2="N10,000" price1="N5,000" img={okro}/>
                 <Food name="Vegetable (Beef or Goat Meat)" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N16,000" price2="N13,000" price1="N6,000" img={vegetable}/>
                 <Food name="White Soup (Goat & Assorted Meat)" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N16,000" price2="N13,000" price1="N6,000" img={whitesoup}/>
-                <Food name="Banga Soup" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N16,000" price2="N13,000" price1="N6,000" img={}/>
+                <Food name="Banga Soup" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N16,000" price2="N13,000" price1="N6,000" img={banga}/>
 
                 <Food name="Tomato Stew (Goat & Goat Meat)" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N15,000" price2="N10,000" price1="N5,000" img={}/>
                 <Food name="Tomato Stew (Chicken)" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N16,000" price2="N12,000" price1="N6,000" img={}/>

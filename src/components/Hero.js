3db28e6ddd18b5b1415ca73e1ecf8cf6c07939ec
patrickodey <patrickodey419@gmail.com>
2021-11-01 23:44:57 +0100

    vegetable

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,6 +8,7 @@ import bg3 from '../images/bg3.jpg';
 import afang from '../images/afang.jpg';
 import egusi from '../images/egusi.jpg';
 import okro from '../images/egusi.jpg';
+import vegetable from '../images/vegetable.jpg';
 
 function Hero() {
   return (
@@ -69,7 +70,7 @@ function Hero() {
                 <Food name="Afang(Beef or Goat Meat)" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N16,000" price2="N13,000" price1="N6,000" img={afang}/>
                 <Food name="Egusi (Beef or Goat Meat)" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N15,000" price2="N10,000" price1="N5,000" img={egusi}/>
                 <Food name="Okra (Beef or Goat Meat)" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N15,000" price2="N10,000" price1="N5,000" img={okro}/>
-                <Food name="Vegetable (Beef or Goat Meat)" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N16,000" price2="N13,000" price1="N6,000" img={}/>
+                <Food name="Vegetable (Beef or Goat Meat)" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N16,000" price2="N13,000" price1="N6,000" img={vegetable}/>
                 <Food name="White Soup (Goat & Assorted Meat)" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N16,000" price2="N13,000" price1="N6,000" img={}/>
                 <Food name="Banga Soup" quantity1="1 Litre" quantity2="3 Litres" quantity3="5 Litres" price3="N16,000" price2="N13,000" price1="N6,000" img={}/>
 

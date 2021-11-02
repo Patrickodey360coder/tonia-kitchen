4775dd9d314031fefcd4adeb6bbac3dced04fd08
patrickodey <patrickodey419@gmail.com>
2021-11-02 01:51:12 +0100

    contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Contact() {
   return (
     <div className="container">
-      <h1>This is my contact page</h1>
+      <h2 className="text-center orange">This is my contact page</h2>
     </div>
   )
 }

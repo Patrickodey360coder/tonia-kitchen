721ad806314ba0b6e3d3fb7719db54d615aeadfc
patrickodey <patrickodey419@gmail.com>
2021-11-02 02:52:19 +0100

    contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,10 +2,32 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div className="container">
-      <h2 className="text-center orange">This is my contact page</h2>
+    <div>
+      <div className="py-5">
+        <h2 className="text-center orange">Contact page</h2>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 mx-auto">
+          <form className="py-5 mb-5">
+            <div className="mb-3">
+              <label for="name" className="form-label">Name</label>
+              <input type="text" className="form-control" id="name" />
+            </div>
+            <div className="mb-3">
+              <label for="exampleInputEmail1" className="form-label">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+            </div>
+            <div className="mb-3">
+              <textarea className="form-control" placeholder="Leave a message here" id="floatingTextarea2"></textarea>
+            </div>
+            <button type="submit" className="btn bg-orange">Submit</button>
+          </form>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Contact
+export default Contact;

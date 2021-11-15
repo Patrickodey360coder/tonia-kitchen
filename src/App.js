12e48394d15cca7d5b0f04548dd3aa5063b89821
patrickodey <patrickodey419@gmail.com>
2021-11-15 23:23:15 +0100

    json update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from './components/Navbar';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import Globalstyle from "./Globalstyle";
 import Hero from './components/Hero';
 import About from './pages/About';
@@ -19,10 +19,10 @@ function App() {
             <Route exact path="/">
               <Hero/>
             </Route>
-            <Route path="/about">
+            <Route exact path="/about">
               <About/>
             </Route>
-            <Route path="/contact">
+            <Route exact path="/contact">
               <Contact/>
             </Route>
             <Route>
